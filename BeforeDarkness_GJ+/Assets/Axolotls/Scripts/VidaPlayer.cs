ca2b562b4f69790c727c42cb3fe3bfab5c228a9c
Jorge Salas <cdmx2767@amerike.edu.mx>
2024-11-11 20:58:57 -0600

    Arreglando mas codigos

---
+++ BeforeDarkness_GJ+/Assets/Axolotls/Scripts/VidaPlayer.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VidaPlayer : MonoBehaviour
+{
+    public CharacterController CC;
+    public Slider VidaVela;
+    public float increasespeed;
+    public float decreasespeed;
+    public float decreasesprint;
+    public bool isRecharging = false;
+    private float stop = 0.2f;
+
+    void Start()
+    {
+        CC = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RecargarVela();
+        BajarVela();
+    }
+
+    void RecargarVela()
+    {
+        /*if (Inventory2.Cerillos > 0)
+        {
+            if (Input.GetKey(KeyCode.R) && VidaVela.value < VidaVela.maxValue)
+            {
+                isRecharging = true;
+            }
+        }*/
+    }
+
+
+    void BajarVela()
+    {
+        if (VidaVela.value <= 100 && Input.GetAxis("Run") > 0)
+        {
+            VidaVela.value -= (decreasesprint * decreasespeed) * Time.deltaTime;
+        }
+        else if (VidaVela.value <= 100)
+        {
+            VidaVela.value -= decreasespeed * Time.deltaTime;
+        }
+    }
+}
