fffdb72e5f9a3c99270c4867387be70d47ad2a0b
Jorge Salas <cdmx2767@amerike.edu.mx>
2024-12-10 13:56:02 -0600

    continuando con los scripts

--- BeforeDarkness_GJ+/Assets/Axolotls/Scripts/MovPlayer.cs
+++ BeforeDarkness_GJ+/Assets/Axolotls/Scripts/MovPlayer.cs
@@ -3,32 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(Inventory))]
 public class MovPlayer : MonoBehaviour
 {
-    Inventory inv;
     public CharacterController CC;
     public float Speed;
     private float sprint = 1.5f;
-
-    public Slider RecargaVela;
-    public float increasespeed;
-    public float decreasespeed;
-    public float decreasesprint;
     public bool isRecharging = false;
     private float stop = 0.2f;
 
     void Start()
     {
         CC = GetComponent<CharacterController>();
-        inv = GetComponent<Inventory>();
     }
 
     void Update()
     {
         Movimiento();
-        RecarVela();
-        DecVela();
     }
 
 
@@ -55,30 +45,6 @@ public class MovPlayer : MonoBehaviour
         if (movimiento != Vector3.zero)
         {
             CC.Move(movimiento * velocidadMovimiento * Time.deltaTime);
-        }
-    }
-
-    void RecarVela()
-    {
-        if (Inventory.Cerillos > 0)
-        {
-            if (Input.GetKey(KeyCode.R) && RecargaVela.value < RecargaVela.maxValue)
-            {
-                isRecharging = true;
-            }
-        }
-    }
-
-
-    void DecVela()
-    {
-        if (RecargaVela.value <= 100 && Input.GetAxis("Run") > 0)
-        {
-            RecargaVela.value -= (decreasesprint * decreasespeed) * Time.deltaTime;
-        }
-        else if (RecargaVela.value <= 100)
-        {
-            RecargaVela.value -= decreasespeed * Time.deltaTime;
         }
     }
 }
--- BeforeDarkness_GJ+/Assets/Axolotls/Scripts/VidaPlayer.cs
+++ BeforeDarkness_GJ+/Assets/Axolotls/Scripts/VidaPlayer.cs
@@ -1,18 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Inventory))]
 public class VidaPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Inventory inv;
+    MovPlayer movPlayer;
+
+    public Slider RecargaVela;
+    public float increasespeed;
+    public float decreasespeed;
+    public float decreasesprint;
+
     void Start()
     {
-        
+        inv = GetComponent<Inventory>();
+        movPlayer = GetComponent<MovPlayer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RecarVela();
+        DecVela();
+
+        if (movPlayer.isRecharging)
+        {
+            RecargaVela.value += increasespeed * Time.deltaTime;
+
+            if (RecargaVela.value >= RecargaVela.maxValue)
+            {
+                RecargaVela.value = RecargaVela.maxValue;
+                movPlayer.isRecharging = false;
+                Inventory.Cerillos -= 1;
+            }
+        }
+    }
+
+    void RecarVela()
+    {
+        if (Inventory.Cerillos > 0)
+        {
+            if (Input.GetKey(KeyCode.R) && RecargaVela.value < RecargaVela.maxValue)
+            {
+                movPlayer.isRecharging = true;
+            }
+        }
+    }
+
+
+    void DecVela()
+    {
+        if (RecargaVela.value <= 100 && Input.GetAxis("Run") > 0)
+        {
+            RecargaVela.value -= (decreasesprint * decreasespeed) * Time.deltaTime;
+        }
+        else if (RecargaVela.value <= 100)
+        {
+            RecargaVela.value -= decreasespeed * Time.deltaTime;
+        }
     }
 }
